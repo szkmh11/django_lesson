e0d95cd30d5ae31ec6ecc9da39ca167f7c221599
szlmh11 <szkmh11@gmail.com>
2024-09-28 22:23:43 +0900

    最新

--- django_lesson/urls.py
+++ django_lesson/urls.py
@@ -21,5 +21,5 @@ from lesson_project import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.TopView.as_view(), name="top"),
-    path('clad/', views.ProductListView.as_view(), name="list"),
+    path('crad/', views.ProductListView.as_view(), name="list"),
 ]
