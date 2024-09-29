a5b58b6bc5177133f1ce72925e1afb38fbbc8260
szlmh11 <szkmh11@gmail.com>
2024-09-29 18:54:59 +0900

    koushin

--- Django_lesson/app/models.py
+++ Django_lesson/app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+     name = models.CharField(max_length=200)
+     price = models.PositiveIntegerField()
+     maker = models.CharField(max_length=255, default="Unknown")
+
+     class Meta:
+          db_table = 'product'
