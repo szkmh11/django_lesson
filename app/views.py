f933d727598725ae453ab1dc9e753337a5b120d5
szlmh11 <szkmh11@gmail.com>
2024-10-06 18:39:25 +0900

    koushin

---
+++ app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView, ListView
+from .models import Product
+
+class TopView(TemplateView):
+    template_name = "top.html"
+
+class ProductListView(ListView):
+    model = Product
+    template_name = "app.html"
