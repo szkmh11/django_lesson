d6f7909d4d694ba708d0b61748544db56e491160
szlmh11 <szkmh11@gmail.com>
2024-10-13 17:37:26 +0900

    koushin

--- Django_Lesson/urls.py
+++ Django_Lesson/urls.py
@@ -19,7 +19,10 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
+    path('', views.product_list, name='product_list'),
+#homeを表示したい場合は↑をコメントアウトしてこっちを戻す
+#    path('', views.home_view, name='home'),
     path('admin/', admin.site.urls),
-    path('', views.TopView.as_view(), name="top"),
-    path('app/' , views.ProductListView.as_view(), name="list")
+    path('app/', views.product_list, name='product_list'),
+    
 ]
--- app/views.py
+++ app/views.py
@@ -1,13 +1,30 @@
+import os
+from django.core.paginator import Paginator
 from django.shortcuts import render
+from django.http import HttpResponse
 from django.views.generic import TemplateView, ListView
 from .models import Product
 
 class TopView(TemplateView):
     template_name = "top.html"
 
-class ProductListView(ListView):
-    model = Product
-    paginate_by = 2
-    template_name = "app.html"
+#class ProductListView(ListView):
+#    model = Product
+#    paginate_by = 2
+#    template_name = "app.html"
 
 #以下自力で関数設定するのです<urls.pyも忘れずに>
+
+def product_list(request):
+    products = Product.objects.all()  # 商品を全件取得
+    paginator = Paginator(products, 2)
+    
+    # 現在のページ番号を取得
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    
+    return render(request, 'app.html', {'page_obj': page_obj})
+
+def home_view(request):
+    return HttpResponse("<h1>ホームページ</h1><p>ようこそ！</p>")
